0979d2418cf88a65cec966446b8c6e3474d12226
alexkch <alexc0219@gmail.com>
2019-02-06 01:35:56 -0500

    added project graphql resolver

--- graphql/resolver.js
+++ graphql/resolver.js
@@ -1,5 +1,17 @@
+const { Project } = require('../db/models');
+const { mongoDbService } = require('../services');
+
 module.exports = {
-  hello() {
-    return { text: 'hello world', views: 3 };
+  project: args => {
+    return mongoCloudService
+      .getById(Project, args.id)
+      .then(result => ({ _id: result._id.toString(), title: result.title }));
+  },
+  projects: () => {
+    return mongoCloudService
+      .getAll(Project)
+      .then(result =>
+        result.map(e => ({ _id: e._id.toString(), title: e.title }))
+      );
   }
 };
